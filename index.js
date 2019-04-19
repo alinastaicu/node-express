8793f636e4eff32d599d07e8e3a570f6f11ab978
Alina Staicu <alina.staicu@check24.de>
2019-04-19 12:51:59 +0200

    Express Simple REST

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 const express = require('express');
 const http = require('http');
-const morgan = require('morgan'); //Morgan is used to log information to the screen
+const morgan = require('morgan'); //morgan is used to log information to the screen
+const bodyParser = require('body-parser');
+
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes', (req, res, next) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  next(); //pass the paramenter to get
+});
+
+app.get('/dishes', (req, res, next) => {
+  res.end('Will send all the dishes to you!');
+});
+
+app.post('/dishes', (req, res, next) => {
+  res.end('Will add the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.put('/dishes', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('PUT operation not supported on /dishes');
+});
+
+app.delete('/dishes', (req, res, next) => {
+  res.end('Deleting all the dishes!');
+});
+
+app.get('/dishes/:dishId', (req, res, next) => {
+  res.end('Will send details of the dish:' + req.params.dishId + ' to you!');
+});
+
+app.post('/dishes/:dishId', (req, res, next) => {
+  res.statusCode = 403;
+  res.end('POST operation not supported on /dishes/' + req.params.dishId);
+});
+
+app.put('/dishes/:dishId', (req, res, next) => {
+  res.write('Updating the dish: ' + req.params.dishId + '\n');
+  res.end('Will update the dish: ' + req.body.name + ' with details: ' + req.body.description);
+});
+
+app.delete('/dishes/:dishId', (req, res, next) => {
+  res.end('Deleting dish: ' + req.params.dishId);
+});
 
 /* This tells Express to serve up the static files from double underscore dirname. 
 This says the root of this project and they will find those files in double_dirname, 
